52837b8340023350675b9acfc2a079a62b2ccc41
josan0112 <trujillohenaresj@gmail.com>
2022-03-21 12:37:06 +0100

    primercommit

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * El CRUD debe contener:
+ * -Opción de añadir un elemento nuevo.
+ * -Opción de eliminar un elemento ya existente.
+ * -Opción de editar un elemento existente.
+ * -Filtro de realizado o no realizado.
+ * -Buscador de palabras.
+ * -Todo debe guardarse en el localStorage
+ */
+

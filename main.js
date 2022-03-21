eec8630f5cec2f32e2655ac15392c61f59449446
josan0112 <trujillohenaresj@gmail.com>
2022-03-21 23:57:05 +0100

    add y remove

--- main.js
+++ main.js
@@ -7,4 +7,17 @@
  * -Buscador de palabras.
  * -Todo debe guardarse en el localStorage
  */
-
+const areaTexto = document.querySelector("#text-area");
+const lista = document.querySelector("#list");
+const arrayElem = [];
+function addElement() {
+    let title = areaTexto.value;
+    arrayElem.push(title);
+    lista.innerHTML = "";
+    arrayElem.forEach((item, index)=>{
+        lista.innerHTML += ` <li id="li-${index}" class="list-element">${item}<button class="delete" onclick="deleteElement(${index})">x</button></li>`
+    })
+}
+function deleteElement(index) {
+    lista.removeChild(lista.querySelector(`#li-${index}`));
+}
